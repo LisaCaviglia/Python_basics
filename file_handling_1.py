63c8d4795456022a2bdd76ebfed0482639d1d710
LisaCaviglia <s4993924@studenti.unige.it>
2025-08-25 21:22:02 +0200

    first try with python

---
+++ file_handling_1.py
@@ -0,0 +1,30 @@
+'''
+Salva numeri e rileggili
+Chiedi all’utente di inserire dei numeri.
+Salvali in un file (numeri.txt).
+Poi riapri il file, leggi i numeri e calcola la somma/media.
+'''
+
+def write_numbers(input_numbers):
+    f = open("numbers.txt", "w")
+    f.write(input_numbers)
+    f.close()
+
+def read_numbers():
+    f = open("numbers.txt", "r")
+    numbers = f.read()
+    numbers = numbers.replace(",", " ") 
+    numbers = list(map(int, numbers.split())) # Converti ogni elemento in intero
+    print("Numeri:", numbers)
+    print("Somma:", sum(numbers))
+    print("Media:", sum(numbers) / len(numbers) if numbers else 0)
+    f.close()
+
+def main():
+    input_numbers = input("Give me some numbers, when you're done, just press enter.\n")
+    write_numbers(input_numbers)
+    read_numbers()
+
+if __name__ == "__main__":
+    main()
+
