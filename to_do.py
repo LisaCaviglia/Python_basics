fae971de6577a5fb0e8aca57b56ab53a4ae7cb7d
LisaCaviglia <s4993924@studenti.unige.it>
2025-08-27 16:38:18 +0200

    to do list

---
+++ to_do.py
@@ -0,0 +1,41 @@
+'''
+Crea una lista to do
+'''
+
+tasks = []
+
+while True:
+    print("To do list:\n1. Add\n2. View\n3. Remove\n4. Exit")
+    choice = input("Choose ad option: ")
+
+    match choice:
+        case "1":
+            task = input("\nWhat you want to add? ").strip()
+            tasks.append(task)
+        case "2":
+            if not tasks: 
+                print("\nSorry, you've not tasks\n")
+            else:
+                counter = 1
+                print("\nTo-do list:")
+                for i in tasks:
+                    print(str(counter) +". " + i + "\n")
+                    counter+=1
+        case "3":
+            if not tasks:
+                print("\nSorry, you've not tasks\n")
+            else:
+                counter = 1
+                print("\nTo-do list:")
+                for i in tasks:
+                    print(str(counter) +". " + i + "\n")
+                    counter+=1
+                rm_task = input("What task you wanto to remove? ").strip()
+                tasks.pop(int(rm_task)-1)
+        case "4":
+            print("Ok Bye!")
+            break
+            
+        
+                
+
